ff9536da8758a6da847d832b58820517952e1b09
daton <tonydangelo123@gmail.com>
2017-05-18 16:26:56 +0200

    added app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './hero/heroes/heroes.component';
+import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/en/dashboard', pathMatch: 'full' },
+  { path: 'en/dashboard', component: DashboardComponent, data: { lang: 'en-UK' } },
+  { path: 'it/cruscotto', component: DashboardComponent, data: { lang: 'it-IT' } },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
